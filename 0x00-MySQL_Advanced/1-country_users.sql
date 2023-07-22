4decbb20c993d4ac7aeb67f638300d2816a6eebf
mazi-kunle <olakunlemichael644@gmail.com>
2023-07-22 16:14:19 +0100

    1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,7 @@
 -- A script that creates a table users
 
-CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS users
+CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NUT NULL UNIQUE,
     name VARCHAR(255),
