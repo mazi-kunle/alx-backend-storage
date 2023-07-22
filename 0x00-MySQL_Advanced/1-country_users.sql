a1fef2b0ca8dd481b0d99ec5e8a9ef0ebdef3bf0
mazi-kunle <olakunlemichael644@gmail.com>
2023-07-22 16:10:38 +0100

    task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- A script that creates a table users
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NUT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL
+);
