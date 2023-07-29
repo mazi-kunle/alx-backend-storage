5421a18e40bca0ba004759f4356aa33deb41cc37
mazi-kunle <olakunlemichael644@gmail.com>
2023-07-29 21:01:52 +0100

    task 11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''task 11 module'''
+
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''
+    a Python function that returns the list of school having a specific topic
+    '''
+    lst = []
+    for i in mongo_collection.find():
+        if topic in i.get('topics', ''):
+            lst.append(i)
+
+    return lst
