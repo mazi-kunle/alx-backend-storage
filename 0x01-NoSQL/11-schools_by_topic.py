defd307890d32878a99553eed0be5c1b5cce5bae
mazi-kunle <olakunlemichael644@gmail.com>
2023-07-30 18:38:06 +0100

    task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,13 +4,24 @@
 import pymongo
 
 
+# def schools_by_topic(mongo_collection, topic):
+#     '''
+#     a Python function that returns the list of school having a specific topic
+#     '''
+#     lst = []
+#     for i in mongo_collection.find():
+#         if topic in i.get('topics', ''):
+#             lst.append(i)
+
+#     return lst
+
+
 def schools_by_topic(mongo_collection, topic):
     '''
     a Python function that returns the list of school having a specific topic
     '''
     lst = []
-    for i in mongo_collection.find():
-        if topic in i.get('topics', ''):
-            lst.append(i)
+    # ans = mongo_collection.find({'topics': {'$elemMatch': {'$eq': topic}}})
+    ans = mongo_collection.find({'topics': topic})
 
-    return lst
+    return [i for i in ans]
