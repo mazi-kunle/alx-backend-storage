28a44711b63be355b37a62698a9b7c0f47614501
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-03 03:18:30 +0100

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 '''This is a module'''
 
 import redis
-from typing import Union
+from typing import Union, Callable
 import uuid
 
 
@@ -22,3 +22,32 @@ class Cache:
         id = str(uuid.uuid4())
         self._redis.set(id, data)
         return id
+
+    def get(
+            self,
+            key: str,
+            fn: Callable = None
+            ) -> Union[str, bytes, int, float]:
+        '''
+         a get method that take a key string argument and
+         an optional Callable argument named fn. This callable
+         will be used to convert the data back to the desired format.
+        '''
+        ans = self._redis.get(key)
+
+        if ans and fn is not None:
+            return fn(ans)
+
+        return ans
+
+    def get_str(self, key: str) -> str:
+        '''
+        parametrize Cache.get to a str
+        '''
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        '''
+        parametrize Cache.get to an int
+        '''
+        return self.get(key, lambda x: int(x))
